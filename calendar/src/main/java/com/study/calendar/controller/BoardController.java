99b6c1889fa1470c1e601a723d9900cdef117863
hyemo0903 <hyemin6647@naver.com>
2024-07-26 17:55:41 +0900

    끝! 집가서 해야지

--- calendar/src/main/java/com/study/calendar/controller/BoardController.java
+++ calendar/src/main/java/com/study/calendar/controller/BoardController.java
@@ -11,53 +11,61 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
- //localhost:8080/board/write
+//localhost:8080/board/write
 public class BoardController {
     @Autowired
     private BoardService boardService;
+
     @GetMapping("/board/write")
-    public String boardWriteForm(){
+    public String boardWriteForm() {
         return "boardWrite";
     }
+
     @PostMapping("/board/writePro")
     public String boardWritePro(Board board, Model model) {
         boardService.write(board);
         model.addAttribute("message", "글 작성이 완료되었습니다.");
         model.addAttribute("url", "/board/list");
-
         return "message";
     }
+
     @GetMapping("/board/list")
-    public String boardList(Model model){
+    public String boardList(Model model) {
         model.addAttribute("list", boardService.boardList());
         return "boardList";
     }
+
     @GetMapping("/board/view") //local:8080/board/view?id=1
-    public String boardView(Model model, @RequestParam("id") Integer id){
-        model.addAttribute("board",boardService.boardView(id));
+    public String boardView(Model model, @RequestParam("id") Integer id) {
+        model.addAttribute("board", boardService.boardView(id));
         return "boardView";
     }
+
     @GetMapping("/board/delete")
-    public String boardDelete(@RequestParam("id") Integer id){
+    public String boardDelete(@RequestParam("id") Integer id, Model model) {
+        model.addAttribute("message", "글이 삭제 되었습니다");
+        model.addAttribute("url", "/board/list");
+
         boardService.boardDelete(id);
         return "message";
 
     }
+
     @GetMapping("/board/modify/{id}")
     //pathvariable 은 modify/ 뒷 부분에 오는 id 부분이 변수로 들어감
-    public String boardModify(@PathVariable("id") Integer id, Model model){
+    public String boardModify(@PathVariable("id") Integer id, Model model) {
         model.addAttribute("board", boardService.boardView(id));
-
-
-return "boardModify";
+        return "boardModify";
     }
+
     @PostMapping("/board/update/{id}")
-    public String boardUpdate(@PathVariable("id") Integer id, Board board){
-       Board boardtemp = boardService.boardView(id);
+    public String boardUpdate(@PathVariable("id") Integer id, Board board,Model model) {
+        Board boardtemp = boardService.boardView(id);
         boardtemp.setTitle(board.getTitle());
         boardtemp.setContent(board.getContent());
-
-boardService.write(boardtemp);
+        boardService.write(boardtemp);
+        model.addAttribute("message", "글이 수정 되었습니다");
+        model.addAttribute("url", "/board/list");
         return "message";
     }
 
